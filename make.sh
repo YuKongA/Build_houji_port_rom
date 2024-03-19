6c34f9eeffe4bdaa128639651d33a4b6558a5f97
余空 <70465933+YuKongA@users.noreply.github.com>
2024-03-19 09:50:12 +0800

    Update KernelSU KSUD

--- make.sh
+++ make.sh
@@ -23,7 +23,6 @@ build_time=$(date) && build_utc=$(date -d "$build_time" +%s)   # 构建时间
 
 magiskboot="$GITHUB_WORKSPACE"/tools/magiskboot
 ksud="$GITHUB_WORKSPACE"/tools/KernelSU/ksud
-lkm="$GITHUB_WORKSPACE"/tools/KernelSU/android14-6.1_kernelsu.ko
 
 a7z="$GITHUB_WORKSPACE"/tools/7zzs
 payload_extract="$GITHUB_WORKSPACE"/tools/payload_extract
@@ -157,7 +156,7 @@ echo -e "${Red} - 添加 KernelSU 支持（可选择）"
 mkdir -p "$GITHUB_WORKSPACE"/init_boot
 cd "$GITHUB_WORKSPACE"/init_boot
 cp -f "$GITHUB_WORKSPACE"/"${device}"/firmware-update/init_boot.img "$GITHUB_WORKSPACE"/init_boot
-$ksud boot-patch -b "$GITHUB_WORKSPACE"/init_boot/init_boot.img -m $lkm --magiskboot $magiskboot
+$ksud boot-patch -b "$GITHUB_WORKSPACE"/init_boot/init_boot.img --magiskboot $magiskboot --kmi android14-6.1
 mv -f "$GITHUB_WORKSPACE"/init_boot/kernelsu_boot*.img "$GITHUB_WORKSPACE"/"${device}"/firmware-update/init_boot-kernelsu.img
 rm -rf "$GITHUB_WORKSPACE"/init_boot
 # 替换 vendor_boot 的 fstab
